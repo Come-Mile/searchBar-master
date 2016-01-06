8348202ecb612d40da2c3a8fb8a6729ff4da928b
Sino <Sino@xiamingwei-NB.local>
2016-01-06 12:15:16 +0800

    Initial Commit

---
+++ mySeachBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mySeachBar
+//
+//  Created by Sino on 16/1/6.
+//  Copyright (c) 2016年 夏明伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mySeachBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mySeachBar
+//
+//  Created by Sino on 16/1/6.
+//  Copyright (c) 2016年 夏明伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
